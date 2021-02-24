0af387db2649fd841283f8d75c0a47c444ac5359
danieljaimes <danieljaimes@192.168.0.9>
2021-02-23 19:49:49 -0600

    Primer commit, refleja solo la edad.

---
+++ Github2Bx/src/github2bx/Github2Bx.java
@@ -0,0 +1,27 @@
+/*
+ * Lenguajes de programación I
+ * Facultad de Ingeniería
+ * Universidad Autónoma de Campeche
+ */
+package github2bx;
+
+/**
+ *
+ * @author danieljaimes
+ */
+public class Github2Bx {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Github");
+        
+        int edad = 23;
+        edad++;
+        
+        System.out.println("edad = " + edad);
+    }
+    
+}
